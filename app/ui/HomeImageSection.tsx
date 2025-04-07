5656655459b597f24fb7f0d30d8a4f6a6fa5faff
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-07 17:34:23 +0200

    add new jpg in public folder and finish to create component HomeImageSection.tsx

--- app/ui/HomeImageSection.tsx
+++ app/ui/HomeImageSection.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function HomeImageSection() {
     return (
-        <p className="bg-blue-500">Salus tous le monde</p>
+        <div className="flex justify-center">
+            <Image src="/nathan-cima-photo.jpg" alt="Photo d'une statut du logo des Jeux Olympiques en plein milieu de Paris - Photo réalisé par Nathan Cima" width={2000} height={1000}/>
+        </div>
     )
 }
