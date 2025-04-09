b44f7b8b28c0e7508b5c42b7a003e2cf953a393c
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-09 16:12:26 +0200

    add new e2e test in app.cy.js to test when a user decide to visit the booking page

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -4,3 +4,10 @@ describe('Visit', () => {
         cy.visit('http://localhost:3000/')
     })
 })
+
+describe('Visit', () => {
+    it('Should visit the booking page', () => {
+        // Visit the index page
+        cy.visit('http://localhost:3000/NosOffres')
+    })
+})
