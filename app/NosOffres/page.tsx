f6c6e6475ce4dfbe4f8cae7ff2138e3ec123f45c
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-09 16:39:34 +0200

    create first components for Booking page

--- app/NosOffres/page.tsx
+++ app/NosOffres/page.tsx
@@ -1,5 +1,10 @@
 export default function Page() {
     return (
-        <p>Dashboard Page</p>
+        <div>
+            <h1>Nos Offres</h1>
+            <div>
+                <div><p>Offre à généré avec un fetch en attente</p></div>
+            </div>
+        </div>
     );
 };
