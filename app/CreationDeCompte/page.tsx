354b7a8d53875397804f17730ad2a0a9d499fd68
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-22 16:20:30 +0200

    add confirmPassword to form CreationDeCompte

--- app/CreationDeCompte/page.tsx
+++ app/CreationDeCompte/page.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Page() {
-    const [form, setForm] = useState({ username: '', alias: '', password: '' });
+    const [form, setForm] = useState({ username: '', alias: '', password: '', confirmPassword: '' });
     const [message, setMessage] = useState('');
     const router = useRouter();
 
@@ -14,53 +14,71 @@ export default function Page() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        const res = await fetch('http://localhost:8080/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(form),
-        });
+        if (form.password === form.confirmPassword) {
+            
+            const res = await fetch('http://localhost:8080/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form),
+            });
+    
+            if (res.ok) {
+                setForm({ username: '', password: '', alias: '', confirmPassword:''});
+                router.push('/Login?success=true&message=Votre%20compte%20a%20%C3%A9t%C3%A9%20cr%C3%A9%C3%A9%20avec%20succ%C3%A8s')
+            } else {
+                const error = await res.text();
+                setMessage(`Erreur: ${error}`)
+            }
 
-        if (res.ok) {
-            setForm({ username: '', password: '', alias: ''});
-            router.push('/Login?success=true&message=Votre%20compte%20a%20%C3%A9t%C3%A9%20cr%C3%A9%C3%A9%20avec%20succ%C3%A8s')
         } else {
-            const error = await res.text();
-            setMessage(`Erreur: ${error}`)
+            setMessage("Veuillez confirmer votre mot de passe.")
         }
     }
 
     return (
-        <div>
-            <h1>Création de Compte</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    name="username"
-                    placeholder="Email"
-                    type="email"
-                    value={form.username}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    name="alias"
-                    placeholder="Nom et Prénom"
-                    value={form.alias}
-                    onChange={handleChange}
-                    required
-                />
-                <input
-                    name="password"
-                    placeholder="Mot de passe"
-                    type="password"
-                    value={form.password}
-                    onChange={handleChange}
-                    required
-                />
-                <button>S&apos;inscrire</button>
-            </form>
-            {message && <p>{message}</p>}
+        <div className="flex justify-center">
+            <div>
+                <h1>Création de Compte</h1>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        name="username"
+                        placeholder="Email"
+                        type="email"
+                        value={form.username}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        name="alias"
+                        placeholder="Nom et Prénom"
+                        value={form.alias}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        name="password"
+                        placeholder="Mot de passe"
+                        type="password"
+                        value={form.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        name="confirmPassword"
+                        placeholder="Confirmer le Mot de passe"
+                        type="password"
+                        value={form.confirmPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                    <button>S&apos;inscrire</button>
+                </form>
+                {message && <p>{message}</p>}
+            </div>
         </div>
     );
 }
