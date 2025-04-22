440f328bf0ffd56b0ed3bce050838a5b7775236e
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-22 17:10:32 +0200

    finish to style form in CreationDeCompte page.tsx

--- app/CreationDeCompte/page.tsx
+++ app/CreationDeCompte/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -15,7 +16,7 @@ export default function Page() {
         e.preventDefault();
 
         if (form.password === form.confirmPassword) {
-            
+
             const res = await fetch('http://localhost:8080/auth/signup', {
                 method: 'POST',
                 headers: {
@@ -38,46 +39,113 @@ export default function Page() {
     }
 
     return (
-        <div className="flex justify-center">
-            <div>
-                <h1>Création de Compte</h1>
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Créer un compte
+                </h1>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    Ou{' '}
+                    <Link href="/Login" className="font-medium text-indigo-600 hover:text-indigo-500">
+                        connectez-vous à votre compte existant
+                    </Link>
+                </p>
             </div>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        name="username"
-                        placeholder="Email"
-                        type="email"
-                        value={form.username}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        name="alias"
-                        placeholder="Nom et Prénom"
-                        value={form.alias}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        name="password"
-                        placeholder="Mot de passe"
-                        type="password"
-                        value={form.password}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        name="confirmPassword"
-                        placeholder="Confirmer le Mot de passe"
-                        type="password"
-                        value={form.confirmPassword}
-                        onChange={handleChange}
-                        required
-                    />
-                    <button>S&apos;inscrire</button>
-                </form>
-                {message && <p>{message}</p>}
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    name="username"
+                                    placeholder="Email"
+                                    type="email"
+                                    value={form.username}
+                                    onChange={handleChange}
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="alias" className="block text-sm font-medium text-gray-700">
+                                Nom et Prénom
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    name="alias"
+                                    placeholder="Nom et Prénom"
+                                    value={form.alias}
+                                    onChange={handleChange}
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                Mot de passe
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    name="password"
+                                    placeholder="Mot de passe"
+                                    type="password"
+                                    value={form.password}
+                                    onChange={handleChange}
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+                                Confirmer le Mot de passe
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    name="confirmPassword"
+                                    placeholder="Confirmer le Mot de passe"
+                                    type="password"
+                                    value={form.confirmPassword}
+                                    onChange={handleChange}
+                                    required
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+                        {message && (
+                            <div className="rounded-md bg-red-50 p-4">
+                                <div className="flex">
+                                    <div className="flex-shrink-0">
+                                        <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 001.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <div className="ml-3">
+                                        <h2 className="text-sm font-medium text-red-800">{message}</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                        <div>
+                            <button 
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Créer mon compte
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
