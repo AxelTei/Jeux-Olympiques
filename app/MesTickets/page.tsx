4a67cca4bfb0eb861bd5bdd52b049a597ff2edc1
Axel Teisseire <mr.teisseire@gmail.com>
2025-05-19 10:41:05 +0200

    correct for ticket in prod

--- app/MesTickets/page.tsx
+++ app/MesTickets/page.tsx
@@ -155,7 +155,7 @@ const Page: React.FC = () => {
                                 <div className="mt-4 flex justify-center">
                                     <div>
                                         <QRCode
-                                            value={`http://localhost:3000/verify-ticket/${ticket.qrCodeKey}` || window.location.origin}
+                                            value={`https://jo-api-9bdf561a2bea.herokuapp.com/verify-ticket/${ticket.qrCodeKey}` || window.location.origin}
                                             size={150}
                                             level="H"
                                         />
